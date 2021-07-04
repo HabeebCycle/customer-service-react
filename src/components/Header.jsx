57f2f5677bf5555fcf28fc3e8d70803833ec11f6
Habeeb Okunade <guessy2k2@yahoo.com>
2021-07-04 15:48:32 +1000

    Adding Button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "./Button";
 
 const Header = (props) => {
   return (
     <header className="header">
       <h1>{props.title}</h1>
+      <Button label="Add" />
     </header>
   );
 };
